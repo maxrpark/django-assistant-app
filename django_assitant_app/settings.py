53e2c43bf5896a85a9c357d4c0550638ff43c4eb
Maxigong <maxi@nolgong.com>
2022-03-12 11:49:17 +0900

    txt

--- django_assitant_app/settings.py
+++ django_assitant_app/settings.py
@@ -1,4 +1,4 @@
-import django_heroku
+import django_on_heroku
 from pathlib import Path
 import os
 import environ
@@ -128,4 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
